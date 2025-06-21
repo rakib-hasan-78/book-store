5185ae3f2c11ce52a839bd0f2cac3d060af0e412
rakib hasan <rakibwebdev78@gmail.com>
2025-06-22 04:33:14 +0600

    project initially done

--- src/pages/PageData/PageData.jsx
+++ src/pages/PageData/PageData.jsx
@@ -6,7 +6,7 @@ const PageData = () => {
     return (
         <div className='w-full h-auto'>
             <PageHero className={`flex items-center justify-center mb-5`}>
-                <h2 className='font-bold capitalize text-3xl'>reading books' page Data</h2>
+                <h2 className='font-bold capitalize xxs:text-xl md:text-3xl'>reading books' page Data</h2>
             </PageHero>
             <PageChart />
         </div>
